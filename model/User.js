59fc3157b6a61dcd53af05a012b51e6516e3e0e6
priyanka749 <priyankabhandari749@gmail.com>
2025-01-30 15:51:16 +0545

    user.js

--- model/User.js
+++ model/User.js
@@ -4,26 +4,31 @@ const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
+        trim: true,
     },
     email: {
         type: String,
         required: true,
         unique: true, 
+        trim: true,
    
     },
     phone_number: {
         type: String,
         required: true,
         unique: true, 
+        trim: true,
     },
     
     location: {
         type: String,
         required: true,
+        trim: true,
     },
     password: {
         type: String,
         required: true,
+        trim: true,
     },
     role:{
         type:String,
