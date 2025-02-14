d17929e8b826c3c7cb42a4c4bb994a7742fc2882
priyanka749 <priyankabhandari749@gmail.com>
2025-02-14 15:53:17 +0545

    app

--- app.js
+++ app.js
@@ -1,90 +1,32 @@
-
-const path = require("path");
-const express = require("express");
-const dotenv = require("dotenv");
-const morgan = require("morgan");
-const colors = require("colors");
-const cookieParser = require("cookie-parser");
-const mongoSanitize = require("express-mongo-sanitize"); // for sql injection
-const helmet = require("helmet");
-const xss = require("xss-clean");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-
-
-
 const express=require("express")
 const connectDb=require("./config/db")
-const UserRouter=require("./routes/UserRoute")
-const ServiceProviderRoute=require("./routes/ServiceProviderRoute")
+const cors=require("cors")
+const AuthRouter=require("../skillseek/routes/AuthRoutes")
 
-
-app.use(cors());
-app.options("*", cors());
-
-// Load env file
-dotenv.config({
-  path: "./config/config.env",
-});
-
-
-// Body parser
-app.use(express.json());
-app.use(cookieParser());
-
-app.use(bodyParser.json({}));
-app.use(bodyParser.urlencoded({ extended: true }));
-
-
-// const GroundRouter=require("./routes/GroundRoute")
 const app=express();
 
 connectDb();
 
+const corsOptions = {
+     origin: "http://localhost:5173",
+     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+     credentials: true,
+     allowedHeaders: ["Content-Type", "Authorization"],
+};
 
-
-
-
+app.use(cors(corsOptions));
 app.use(express.json());
-//mount routers 
-app.use("/api/User",UserRouter );
-app.use("/api/ServiceProvider",ServiceProviderRoute);
 
-//body parser
-app.use(express.json());
-app.use(cookieParser());
 
-// Dev logging middleware
-if (process.env.NODE_ENV === "development") {
-     app.use(morgan("dev"));
-   }
-// app.use("/",()=>{
-//     console.log("you r here")
-// })
+app.use("/api/auth", AuthRouter);
 
-// Sanitize data
-app.use(mongoSanitize());
-
-// Set security headers
-app.use(helmet());
-
-// Prevent XSS attacks
-app.use(xss());
-
-// Set static folder
-app.use(express.static(path.join(__dirname, "public")));
-// app.use(express.static('public'));
 
 
 const port=3000;
-app.listen(port,()=>{
-     console.log(`Server running at http://localhost:${port}`)
-})
 
-// Handle unhandled promise rejections
-process.on("unhandledRejection", (err, promise) => {
-     console.log(`Error: ${err.message}`.red);
-     // Close server & exit process
-     server.close(() => process.exit(1));
-   });
-   
+
+
+app.listen(port, () => {
+     console.log(`Server Running at http://localhost:${port}`);
+});
+
