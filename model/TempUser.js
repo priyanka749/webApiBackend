7ae6177880de194c5111d44c2d3a523d500eab75
priyanka749 <priyankabhandari749@gmail.com>
2025-02-14 15:55:25 +0545

    tempuser

---
+++ model/TempUser.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const tempUserSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+    phone_number: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    role: {
+        type: String,
+        enum: ["Customer", "Service Provider"],
+        required: true,
+    },
+    name: {
+        type: String,
+        trim: true,
+    },
+    location: {
+        type: String,
+        trim: true,
+    },
+    image: {
+        type: String,
+    },
+    bio: {
+        type: String,
+    },
+    otp: {
+        type: String,
+    },
+    
+    rating: {
+        type: Number,
+        default: 0,
+    },
+    skills: {
+        type: [String],
+        required: true,
+    },
+});
+
+const TempUser = mongoose.model("tempUsers", tempUserSchema);
+module.exports = TempUser;
